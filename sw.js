9556dba3a54f1d6a6679a60e5205dfd6eb2debe8
Huadi Wang <wanghuadi0145@gmail.com>
2022-01-24 22:02:51 -0800

    change sw.js file

--- sw.js
+++ sw.js
@@ -1,4 +1,7 @@
 self.addEventListener("install", e => {
-    console.log("Install!");
+    e.waitUntil(
+        caches.open("static").then(cache => {
+            return cache.addAll(["./", "./src/master.css", "./images/logo192.png"]);
+        })
+    );
 });
-
